b34d8f6f8d18a9d9c393cb2bfa17429e9d84506c
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-05-26 21:29:00 +0700

    Initial Commit

---
+++ GitPlayground/GitPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPlaygroundApp.swift
+//  GitPlayground
+//
+//  Created by Anthony on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GitPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
